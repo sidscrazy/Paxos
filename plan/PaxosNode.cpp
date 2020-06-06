3072ca8e3942fc30d17b59c12ebb6509f017ece0
sidscrazy <sidscrazy@gmail.com>
2020-06-06 01:20:56 -0700

    added guid generation function

--- plan/PaxosNode.cpp
+++ plan/PaxosNode.cpp
@@ -1,11 +1,16 @@
-// This class is used to create an instance of a node
+// This class is used to create an instance of a node. Only for testing as Jackson has already implemented node class.
 // Open: Differentiate N and "red" values. Eg Propose(N) and AcceptReq(N, "red") which one is value?
 // Open: A leader node must know which all other nodes are live, how to store/query this?
+
+#include <sstream>
+#include <random>
+#include <string>
+
+using namespace  std;
 
 class PaxosNode
 {
     // constructors and destructors
-    PaxosNode();
     PaxosNode();
 
     // variables
@@ -29,4 +34,28 @@ class PaxosNode
     int Accept();
     int Broadcast();
 
+    // guid generator
+    unsigned int random_char();
+    std::string generate_hex(const unsigned int len);
 };
+
+unsigned int  PaxosNode::random_char() 
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, 255);
+    return dis(gen);
+}
+
+string PaxosNode::generate_hex(const unsigned int len) 
+{
+    stringstream ss;
+    for (auto i = 0; i < len; i++) {
+        const auto rc = random_char();
+        stringstream hexstream;
+        hexstream << hex << rc;
+        auto hex = hexstream.str();
+        ss << (hex.length() < 2 ? '0' + hex : hex);
+    }
+    return ss.str();
+}
